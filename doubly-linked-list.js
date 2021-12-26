a9be6f3e76e7ef8db3ae825f6d643ab2be642e2e
irtizaaah <irtizaaah@gmail.com>
2021-12-26 21:30:14 +0400

    Created doubly-linked-list.js

---
+++ doubly-linked-list.js
@@ -0,0 +1,37 @@
+import {SinglyLinkedList} from "./singly-linked-list.js"
+
+export class DoublyLinkedList extends SinglyLinkedList{
+    constructor(list){
+        super(list);
+    }
+
+    _createDefaultNodes(){
+        this.head = this._createNewNode(this.head); // this is a dummy head (doesn't store a value)
+        this.tail = null; // No tail is being used
+
+    }
+
+    _linkNodes(leftNode, rightNode){
+        leftNode.setNext = rightNode;
+        rightNode.setPrevious = leftNode;
+    }
+
+    print(){
+        let currentNode = this.head;
+        let printedList = "head->";
+        
+        while(currentNode.getNext !== null){
+            currentNode = currentNode.getNext;
+
+            if(currentNode.getPrevious !== null){
+                printedList = printedList + "<-" + currentNode.getValue + "->";
+            }
+            else{
+                printedList = printedList + currentNode.getValue + "->";
+            }
+        }
+
+        printedList = printedList + "null";
+        console.log(printedList);
+    }
+}
