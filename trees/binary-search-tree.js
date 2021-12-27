d24f3b4e5253beccb8395c8971f8f28f0542c3bf
irtizaaah <irtizaaah@gmail.com>
2021-12-27 19:55:14 +0400

    Created tree skeleton

---
+++ trees/binary-search-tree.js
@@ -0,0 +1,38 @@
+import {Tree} from "./tree.js"
+
+export class BinarySearchTree extends Tree{
+    constructor(){
+        super();
+
+        this.isFull = false;
+        this.isBalanced = false;
+        this.isCompleted = false;
+        this.isPerfect = false;
+    }
+
+    _traverseValues(value, currentNode){ 
+    }
+
+    _traversesNode(newNode, currentNode){
+    }
+
+    insert(value){
+
+    remove(value){
+    }
+
+    search(value){
+    }
+
+    printPreOrder(){
+    }
+
+    printInOrder(){
+    }
+
+    printPostOrder(){
+    }
+
+    properties(){
+    }
+}
---
+++ trees/tree.js
@@ -0,0 +1,31 @@
+import {TreeNode} from "./tree-node.js"
+
+export class Tree{
+    constructor(){
+        this.root = this._createNewNode(null);
+        this.height = 0;
+    }
+
+    get getHeight(){
+        return this.height;
+    }
+
+    _createNewNode(value = null){
+        return new TreeNode(value, null, null);
+    }
+
+    _linkNodes(firstNode, secondNode){
+        firstNode = secondNode;
+    }
+
+    _deleteNode(targetNode){
+        
+    }
+
+    _updateHeight(){
+        this.height = this.height + 1;
+    }
+
+    _traverseValues(){
+    }
+}
